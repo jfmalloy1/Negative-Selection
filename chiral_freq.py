f7f81505bb08f9aecce433e6253e7d90905b3f3e
John <jfmalloy1@gmail.com>
2019-03-11 11:39:21 -0700

    Chirality analyses

--- chiral_freq.py
+++ chiral_freq.py
@@ -1,12 +1,12 @@
 from collections import Counter
 
-s = list("ch3n")
+s = list("O=C1NC(N)=NC2=C1[N+]3=CN(C4=CC=C(C(NC(C(O)=O)CCC(O)=O)=O)C=C4)CC3CN2")
 n = ''
 
 for i in s:
     if i.isalpha():
         n = n+i
-    else:
+    elif i.isdigit():
         n = n + str(n[-1] * (int(i)-1))
 
 print Counter(n)

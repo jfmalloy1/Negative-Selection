63f62536c6a9abc1c0598c9acacaa612a0288c82
John <jfmalloy1@gmail.com>
2019-03-11 12:18:44 -0700

    Updated chiral frequency analysis

--- chiral_freq.py
+++ chiral_freq.py
@@ -1,12 +1,31 @@
 from collections import Counter
+import csv
+import re
 
-s = list("O=C1NC(N)=NC2=C1[N+]3=CN(C4=CC=C(C(NC(C(O)=O)CCC(O)=O)=O)C=C4)CC3CN2")
-n = ''
+#99% of all organic compounds are made of these six elements
+elements= ["H", "C", "N", "O", "Ca", "P"]
+total_count = Counter()
 
-for i in s:
-    if i.isalpha():
-        n = n+i
-    elif i.isdigit():
-        n = n + str(n[-1] * (int(i)-1))
+with open("KEGGchiralityanalysis.csv") as csvfile:
+    readCSV = csv.reader(csvfile)
+    readCSV.next()
+    readCSV.next()
+    for row in readCSV:
+        #Row 16 contains the chiral-determining format
+        s = list(row[16])
+        n = ''
 
-print Counter(n)
+        #Only count the frequency if the compound is chiral (represented by row[6] being greater than 0, meaning it has at least 1 chiral center)
+        if (int(row[6]) > 0):
+            for i in range(len(s)):
+                if s[i] in elements:
+                    if i+1 < len(s) and s[i+1].isdigit():
+                        n = n + (s[i]*int(s[i+1]))
+                    else:
+                        n = n + s[i]
+
+        n = re.findall('[A-Z][^A-Z]*', n)
+        #all_elements.append(n)
+        total_count += Counter(n)
+
+    print total_count
